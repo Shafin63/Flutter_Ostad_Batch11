6d2f96d4e17bcac164ff8f2e8d51f3cbb1858008
Shafin <mohammadshafin63@gmail.com>
2025-06-28 23:06:27 +0600

    week 5 assignment

--- Module 2/class_1_map.dart
+++ Module 2/class_1_map.dart
@@ -1,6 +1,7 @@
 main ()
 {
-  Map <String, String> person = { //Map <key, value>
+  Map <String, String> person =
+  { //Map <key, value>
     'name' : 'Shafin',
     'age' : '25',
     'address' : 'Chittagong',
